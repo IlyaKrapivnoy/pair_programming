9032b2e267943331b3ef8ef0b3656ff9fefd9f53
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2021-11-04 14:19:43 +0200

    add Log In / Log Out btn

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,18 @@
 import { AppBar, Toolbar, Typography, Button } from '@material-ui/core';
 
 const Header = ({ title }) => {
+    const isLoggedIn = true;
+
     return (
         <div>
             <AppBar position='static'>
                 <Toolbar>
                     <Typography variant='h6'>{title}</Typography>
-                    <Button color='inherit'>Login</Button>
+                    {isLoggedIn ? (
+                        <Button color='inherit'>Log Out</Button>
+                    ) : (
+                        <Button color='inherit'>Log In</Button>
+                    )}
                 </Toolbar>
             </AppBar>
         </div>
