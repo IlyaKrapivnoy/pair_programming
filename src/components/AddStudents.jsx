9e379594e24da0ded474690dfe69b2c50905749c
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2021-11-04 15:19:15 +0200

    change pages bg

--- src/components/AddStudents.jsx
+++ src/components/AddStudents.jsx
@@ -2,7 +2,7 @@ import Header from './Header';
 
 const AddStudents = () => {
     return (
-        <div className='w-10/12'>
+        <div className='w-10/12 bg-gray-100'>
             <Header
                 title='Add Students'
                 description='You can add your students here'
--- src/components/Students.jsx
+++ src/components/Students.jsx
@@ -2,7 +2,7 @@ import Header from './Header';
 
 const Students = () => {
     return (
-        <div className='w-10/12'>
+        <div className='w-10/12 bg-gray-100'>
             <Header
                 title='Students'
                 description='You can see the list of your students here'
