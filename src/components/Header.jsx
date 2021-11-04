26d80f09edafb43c4fbd88e649c71774d1fdbd57
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2021-11-04 15:09:04 +0200

    change teacher name when you log in and out

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,6 @@ import { AppBar, Toolbar, Typography, Button } from '@material-ui/core';
 import { useState } from 'react';
 
 const Header = ({ title, description }) => {
-    // const isLogged = true;
     const [isLogged, setIsLogged] = useState(false);
 
     return (
@@ -22,23 +21,58 @@ const Header = ({ title, description }) => {
                             {description}
                         </Typography>
                     </div>
+                    <div className='flex items-center'>
+                        {isLogged ? (
+                            <div className=''>
+                                <Typography
+                                    variant='subtitle1'
+                                    className='text-gray-50'
+                                >
+                                    Tomas Edison
+                                </Typography>
+                                <Typography
+                                    variant='caption'
+                                    className='text-gray-200'
+                                >
+                                    React JS
+                                </Typography>
+                            </div>
+                        ) : (
+                            <div className=''>
+                                <Typography
+                                    variant='subtitle1'
+                                    className='text-gray-50'
+                                >
+                                    Teacher Name
+                                </Typography>
+                                <Typography
+                                    variant='caption'
+                                    className='text-gray-200'
+                                >
+                                    Subject
+                                </Typography>
+                            </div>
+                        )}
 
-                    {isLogged ? (
-                        <Button
-                            variant='outlined'
-                            color='inherit'
-                            onClick={() => setIsLogged(!isLogged)}
-                        >
-                            Log Out
-                        </Button>
-                    ) : (
-                        <Button
-                            variant='contained'
-                            onClick={() => setIsLogged(!isLogged)}
-                        >
-                            Log In
-                        </Button>
-                    )}
+                        {isLogged ? (
+                            <Button
+                                variant='outlined'
+                                color='inherit'
+                                onClick={() => setIsLogged(!isLogged)}
+                                style={{ marginLeft: 30, width: 100 }}
+                            >
+                                Log Out
+                            </Button>
+                        ) : (
+                            <Button
+                                variant='contained'
+                                onClick={() => setIsLogged(!isLogged)}
+                                style={{ marginLeft: 30, width: 100 }}
+                            >
+                                Log In
+                            </Button>
+                        )}
+                    </div>
                 </Toolbar>
             </AppBar>
         </div>
