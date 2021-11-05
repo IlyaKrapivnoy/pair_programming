ab1e6ce26dd3056ef520e4e5ac0167429bc2f111
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2021-11-05 11:54:11 +0200

    display topics

--- src/components/Topics.jsx
+++ src/components/Topics.jsx
@@ -1,6 +1,20 @@
 import Header from './Header';
+import { useEffect, useState } from 'react';
 
 const Topics = ({ isLogged, setIsLogged }) => {
+    const [topic, setTopic] = useState('');
+
+    useEffect(() => {
+        fetch('http://localhost:8000/students')
+            .then((res) => {
+                return res.json();
+            })
+            .then((data) => {
+                setTopic(data);
+                // console.log('data', data);
+            });
+    }, []);
+
     return (
         <div className='w-10/12 bg-gray-100'>
             <Header
@@ -13,6 +27,9 @@ const Topics = ({ isLogged, setIsLogged }) => {
                 isLogged={isLogged}
                 setIsLogged={setIsLogged}
             />
+            {topic.map((topic) => (
+                <div key={topic.id}>{topic.next_topic}</div>
+            ))}
         </div>
     );
 };
